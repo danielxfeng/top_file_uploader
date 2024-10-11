f05daba62a67747979b726e9764213ecb2ba239d
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-10-11 19:46:48 +0300

    AppRouter.

---
+++ controllers/appController.mjs
@@ -0,0 +1,15 @@
+const appGetFiles = async (req, res) => {};
+const appGetNewFile = async (req, res) => {};
+const appPostNewFile = async (req, res) => {};
+const appPutFile = async (req, res) => {};
+const appDelFile = async (req, res) => {};
+const appGetSharedFile = async (req, res) => {};
+
+export {
+  appGetFiles,
+  appGetNewFile,
+  appPostNewFile,
+  appPutFile,
+  appDelFile,
+  appGetSharedFile,
+};
--- routers/appRouter.mjs
+++ routers/appRouter.mjs
@@ -1,4 +1,20 @@
 import { Router } from "express";
+import {
+  appGetFiles,
+  appGetNewFile,
+  appPostNewFile,
+  appPutFile,
+  appDelFile,
+  appGetSharedFile,
+} from "../controllers/appController.mjs";
+
 const appRouter = new Router();
+
+appRouter.get("/files", appGetFiles);
+appRouter.get("/files/new", appGetNewFile);
+appRouter.post("/files/new", appPostNewFile);
+appRouter.put("/files/:fileId", appPutFile);
+appRouter.delete("/files/:fileId", appDelFile);
+appRouter.get("/files/shared/:sharedId", appGetSharedFile);
 
 export default appRouter;
